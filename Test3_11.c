044c1e418d1cde38a94694b5a6b2a5bbf260d89d
林政浩 <1392872316@qq.com>
2022-10-15 17:48:41 +0800

    10_14

--- Test3_11.c
+++ Test3_11.c
@@ -5,17 +5,47 @@ int main(void) {
 	float f_overflow = 3.4E38 * 100;
 	float f_underflow = -3.4E38 * 100;
 
-	printf("整数上溢：%d\n浮点数上溢：%f\n浮点数下溢：%f", i_overflow, f_overflow, f_underflow);
-	//输出结果：
-	//整数上溢： - 100
-	//浮点数上溢：inf
-	//浮点数下溢： - inf
+	printf("整数上溢：%d\n浮点数上溢：%f\n浮点数下溢：%f\n\n", i_overflow, f_overflow, f_underflow);
 
 	//2.
-	char ch;
+	int num;
 
-	scanf("%c", &ch);
-	printf("输入一个");
+	printf("输入一个ASCII码值：__\b\b");
+	scanf("%d", &num);
+	printf("在ASCII码表上对应的字符是：%c\n\n", num);
+
+	//3.
+	printf("\aStartled by the sudden sound, Sally shouted,\n\"By the Great Pumpkin, what was that!\"\n\n");
+
+	//4.
+	float f;
+
+	printf("Enter a  floating-point value：");
+	scanf("%f", &f);
+	printf("fixed-point notation：%f\n", f);
+	printf("exponential notation：%e\n", f);
+	printf("p notation：%.2a\n\n", f);
+
+	//5.
+	int age;
+
+	printf("请输入你的年龄：");
+	scanf(" %age", &age);
+	printf("你已经活了%e秒了！\n\n", age * 3.156E7);
+
+	//6.
+	int kuatuo;
+
+	printf("请输入水的夸脱数：");
+	scanf(" %kuatuo", &kuatuo);	//连续的printf和scanf会导致跳过scanf，可在%kuatuo前面加个空格或\n刷新输出。
+	printf("水分子的数量为：%ld个\n\n", kuatuo * 950 / 3.0E-23);
+
+	//7.
+	float inch;
+
+	printf("输入身高（英寸）：");
+	scanf("%inch", &inch);
+	printf("你的身高是%.2f厘米\n\n", inch * 2.54);
 
 	return 0;
 }
